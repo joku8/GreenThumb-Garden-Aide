12c999bcc514b2123046334820b113d4161a381f
joku8 <jsku2@illinois.edu>
2023-07-22 22:54:01 -0500

    create global vars

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {
   Grid
 } from '@mui/material';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import HarvestTracker from './components/HarvestTracker';
 import Calendar from './components/Calendar';
@@ -10,6 +10,30 @@ import SeedStorage from './components/SeedStorage';
 import TaskManager from './components/TaskManager';
 
 function App() {
+  /** Stores the seeds stored
+   *  Array of objects with the following keys:
+   *  Plant, Cultivar, Source, Year, Notes
+   */
+  const [seedBank, setSeedBank] = useState([]);
+
+  /** Stores garden harvest
+   *  Array of objects with the following keys:
+   *  Item, Date, Amount, Units, Notes
+   */
+  const [harvestBook, setHarvestBook] = useState([]);
+
+  /** Stores garden tasks
+   *  Array of objects with the following keys:
+   *  Todo, Due, Notes
+   */
+  const [taskList, setTaskList] = useState([]);
+
+  /** Stores calendar items
+   *  Array of objects with the following keys:
+   *  Plant, periods (an array of objects showing dates. For example march 15 - april 10: sow, april 10 - June 10 harvest, etc.)
+   */
+  const [calendarItems, setCalendarItems] = useState([]);
+
   return (
     <div>
       <Grid container 
@@ -20,16 +44,28 @@ function App() {
           <Header />
         </Grid>
         <Grid item xs={6}>
-          <SeedStorage />
+          <SeedStorage 
+            seeds={seedBank}
+            setSeeds={setSeedBank}
+          />
         </Grid>
         <Grid item xs={6}>
-          <HarvestTracker />
+          <HarvestTracker
+            harvest={harvestBook}
+            setHarvest={setHarvestBook}
+           />
         </Grid>
         <Grid item xs={8}>
-          <Calendar />
+          <Calendar 
+            calendarList={calendarItems}
+            setCalendarList={setCalendarItems}
+          />
         </Grid>
         <Grid item xs={4}>
-          <TaskManager />
+          <TaskManager 
+            tasks={taskList}
+            setTasks={setTaskList}
+          />
         </Grid>
       </Grid>
     </div>
